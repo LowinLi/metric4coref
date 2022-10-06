1fcf14b24987d6e23a6b6933a847453a61843db9
lowinli <33021512+LowinLi@users.noreply.github.com>
2022-10-06 11:30:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metric4coref",
-    version="0.0.1",
+    version="0.0.2",
     author="LowinLi",
     author_email="idealway9@gmail.com",
     description="metric for coreference resolution",
